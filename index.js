e053a61e4f3f019e8f9ba31bd8b512d55a5a1c60
Pat  Pignataro <pat6284@msn.com>
2018-01-23 19:13:05 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 const app = "I don't do much.";
+
+  const token = 'I1would2rather3not4put5personal6info7in8here9I10hope11that12is13ok'
+fetch('https://api.github.com/user/repos', {
+  headers: {
+    Authorization: `token ${token}`
+  }
+}).then(res => res.json()).then(json => console.log(json));
